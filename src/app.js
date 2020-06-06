f7105b3e30f36ba8b45c722b67ab7a6458c6c743
uehr <uehara.yudai@gmail.com>
2020-06-07 03:35:11 +0900

    add: experiences

--- src/app.js
+++ src/app.js
@@ -16,11 +16,32 @@ function App() {
             "image": site_icon
           }
         }
+        is_first={true}
         image={my_icon}
         body={
           <p className="text-center">
             オプティム社エンジニア / N高等学校OB / 2002年沖縄県生まれ
           </p>
+        }
+      />
+      <Section
+        headline={
+          {
+            "title": "Experiences",
+            "fontawesome": ["fas", "shoe-prints"]
+          }
+        }
+        body={
+          <div>
+            <List body="2015 沖縄海洋ロボコンROV部門 特別賞" />
+            <List body="2016 Google ウェブレンジャープロジェクト 奨励賞" />
+            <List body="2017 N高等学校 入学" />
+            <List body="2018 株式会社ドワンゴ インターン" />
+            <List body="2019 GMOペパボ株式会社 インターン" />
+            <List body="2019 株式会社オプティム インターン" />
+            <List body="2020 N高等学校 卒業" />
+            <List body="2020 株式会社オプティム 入社" />
+          </div>
         }
       />
     </div>
