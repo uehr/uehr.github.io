9e5b8fe4f124542d964795b9f43edb2ff0974ff1
uehr <uehara.yudai@gmail.com>
2020-06-06 23:41:10 +0900

    add: headline comp

---
+++ src/components/atoms/headline.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import icon from "../../assets/img/icon.png"
+import '../../assets/css/headline.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Headline(props) {
+    return (
+        <div className="Headline">
+            <p className="dark-font-color headline-body">
+                {props.fontawesome &&
+                    <FontAwesomeIcon icon={props.fontawesome} className="headline-fontawesome" />
+                }
+                {props.image &&
+                    <img src={props.image} className="headline-image"></img>
+                }
+                {props.title}
+            </p>
+        </div>
+    );
+}
+
+export default Headline;
