aca4a084b2150b32c48e3201182ddac204b14c28
uehr <uehara.yudai@gmail.com>
2020-06-07 02:15:10 +0900

    fix: ルート意外へのアクセスでも表示される

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './assets/css/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -11,14 +12,11 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons'; import { far } from '@fortawesome/free-regular-svg-icons';
 library.add(fab, fas, far);
 
+const root = document.getElementById('root')
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Router>
+    <Route exact path="/" component={App}></Route>
+  </Router>,
+  root);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
