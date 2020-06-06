fb5b187b5f86398452380bace1bde6f5524da0b6
uehr <uehara.yudai@gmail.com>
2020-06-07 05:07:25 +0900

    add: p-large

--- src/components/atoms/sns-link.js
+++ src/components/atoms/sns-link.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function SNSLink(props) {
     return (
         <a className="SNSLink" href={props.link} style={{ color: props.color }}>
-            <p className="sns-link-text">
+            <p className="sns-link-text p-large">
                 <FontAwesomeIcon icon={props.fontawesome} />
                 {props.text}
             </p>
