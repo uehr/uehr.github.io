3506950a9bff19139f4d32b79876e0a99929cfac
uehr <uehara.yudai@gmail.com>
2020-06-07 05:06:23 +0900

    add: hobbies

--- src/app.js
+++ src/app.js
@@ -108,6 +108,30 @@ function App() {
           </div>
         }
       />
+      <Section
+        headline={
+          {
+            "title": "My Hobbies",
+            "fontawesome": ["fas", "walking"]
+          }
+        }
+        body={
+          <div className="text-center">
+            <p className="d-inline-block mx-3 p-large">
+              <FontAwesomeIcon icon={["fas", "walking"]} /> 散歩
+            </p>
+            <p className="d-inline-block mx-3 p-large">
+              <FontAwesomeIcon icon={["fas", "heartbeat"]} /> 筋トレ
+            </p>
+            <p className="d-inline-block mx-3 p-large">
+              <FontAwesomeIcon icon={["fas", "book"]} /> 読書
+            </p>
+            <p className="d-inline-block mx-3 p-large">
+              <FontAwesomeIcon icon={["fas", "laptop-code"]} /> 個人開発
+            </p>
+          </div>
+        }
+      />
     </div>
   );
 }
