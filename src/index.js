e5d1ed0bce1264714b9d9a993dd39d40d6f2da71
uehr <uehara.yudai@gmail.com>
2020-06-07 00:08:32 +0900

    add: import fontawesome

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
 
+// fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons'; import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fab, fas, far);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
