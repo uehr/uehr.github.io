314c6d597a1c9c35ac4ebc177211d355cbd96158
uehr <uehara.yudai@gmail.com>
2020-06-07 09:23:30 +0900

    update: 「uehara」をicon下へ

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './assets/css/app.css';
-import Header from './components/molecules/header.js';
+import Headline from './components/atoms/headline.js';
 import Section from './components/molecules/section.js';
 import List from './components/atoms/list.js'
 import SNSLink from './components/atoms/sns-link.js'
@@ -21,18 +21,18 @@ function App() {
   return (
     <div className="App">
       <Section
-        headline={
-          {
-            "title": "uehara",
-            "image": site_icon
-          }
-        }
         is_first={true}
         image={my_icon}
         body={
-          <p className="text-center">
-            オプティム社エンジニア / N高等学校OB / 2002年沖縄県生まれ
-          </p>
+          <div className="text-center">
+            <Headline
+              title="uehara"
+              image={site_icon}
+            />
+            <p className="text-center">
+              オプティム社エンジニア / N高等学校OB / 2002年沖縄県生まれ
+            </p>
+          </div>
         }
       />
       <Section
--- src/components/molecules/section.js
+++ src/components/molecules/section.js
@@ -4,16 +4,14 @@ import Headline from '../atoms/headline.js';
 
 function Section(props) {
     return (
-        <div className={`Section ${props.is_first ? "" : "section-not-first"}`} >
-            {
-                props.headline.image &&
+        <div className={`Section ${props.is_first ? "" : "section-not-first"} ${props.className ? props.className : ""}`} >
+            {props.headline && props.headline.image &&
                 <Headline
                     title={props.headline.title}
                     image={props.headline.image}
                 />
             }
-            {
-                props.headline.fontawesome &&
+            {props.headline && props.headline.fontawesome &&
                 <Headline
                     title={props.headline.title}
                     fontawesome={props.headline.fontawesome}
