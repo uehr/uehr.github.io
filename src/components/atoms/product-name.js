373b9f98caf638ef46367e1049bebbee584bd48a
uehr <uehara.yudai@gmail.com>
2020-06-07 12:13:27 +0900

    update: プロダクト名の一文字目はテーマカラーへ

--- src/components/atoms/product-name.js
+++ src/components/atoms/product-name.js
@@ -4,9 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function ProductName(props) {
     return (
         <p className="product-name p-large">
-            <FontAwesomeIcon icon={["fas", "chevron-left"]} />
             {props.name}
-            <FontAwesomeIcon icon={["fas", "chevron-right"]} />
         </p >
     );
 }
