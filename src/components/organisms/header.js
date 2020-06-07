08b863de19728b1eeb5e17d5b6f1ba809b9ff35e
uehr <uehara.yudai@gmail.com>
2020-06-07 11:16:22 +0900

    add: headlineに下線を追加

--- src/components/organisms/header.js
+++ src/components/organisms/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../assets/css/section.css';
+import '../../assets/css/header.css';
 import Headline from '../atoms/headline.js';
 import Section from '../molecules/section.js';
 import site_icon from "../../assets/img/site-icon.png"
