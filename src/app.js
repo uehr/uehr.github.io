455ce679d18f64198e3d640ae76d17e5598d5216
uehr <uehara.yudai@gmail.com>
2020-06-07 09:27:35 +0900

    update: experiencesを中央へ

--- src/app.js
+++ src/app.js
@@ -43,15 +43,17 @@ function App() {
           }
         }
         body={
-          <div>
-            <List body="2015 沖縄海洋ロボコンROV部門 特別賞" />
-            <List body="2016 Google ウェブレンジャープロジェクト 奨励賞" />
-            <List body="2017 N高等学校 入学" />
-            <List body="2018 株式会社ドワンゴ インターン" />
-            <List body="2019 GMOペパボ株式会社 インターン" />
-            <List body="2019 株式会社オプティム インターン" />
-            <List body="2020 N高等学校 卒業" />
-            <List body="2020 株式会社オプティム 入社" />
+          <div className="text-center">
+            <div className="d-inline-block text-left">
+              <List body="2015 沖縄海洋ロボコンROV部門 特別賞" />
+              <List body="2016 Google ウェブレンジャープロジェクト 奨励賞" />
+              <List body="2017 N高等学校 入学" />
+              <List body="2018 株式会社ドワンゴ インターン" />
+              <List body="2019 GMOペパボ株式会社 インターン" />
+              <List body="2019 株式会社オプティム インターン" />
+              <List body="2020 N高等学校 卒業" />
+              <List body="2020 株式会社オプティム 入社" />
+            </div>
           </div>
         }
       />
