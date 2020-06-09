8f8e3c73161bc810e6c7ae906173a8b15b172048
uehr <uehara.yudai@gmail.com>
2020-06-09 19:16:31 +0900

    add: langsへrustを追加

--- src/components/organisms/langs.js
+++ src/components/organisms/langs.js
@@ -17,17 +17,18 @@ function Langs() {
                 <div className="lang-section-body d-flex flex-row justify-content-around">
                     <div className="text-left lang-section">
                         <p className="p-large">業務</p>
-                        <List body="Java (Spring)" />
+                        <List body="Java" />
                         <List body="Python" />
-                        <List body="Ruby (Rails)" />
+                        <List body="Ruby" />
                     </div>
 
                     <div className="text-left align-top lang-section">
                         <p className="p-large">個人開発</p>
                         <List body="Nim" />
                         <List body="Go" />
-                        <List body="JS (React/Node.js)" />
+                        <List body="JS" />
                         <List body="C/C++" />
+                        <List body="Rust" />
                     </div>
                 </div>
             }
