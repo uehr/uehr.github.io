37a2083494ba0475919892b05fe2a05b68fd9148
uehr <uehara.yudai@gmail.com>
2020-06-15 20:58:23 +0900

    fix: (スマホ)fieldの表示がはみ出る

--- src/components/molecules/vertical-list.js
+++ src/components/molecules/vertical-list.js
@@ -5,8 +5,8 @@ import List from '../../components/atoms/list.js'
 function VerticalList(props) {
     return (
         <div className={`text-left vertical-list ${props.class}`}>
-            <div className={`large-font ${props.title_class}`}>{props.title}</div>
-            <div className={`${props.list_class}`}>
+            <div className={`large-font vertical-list-title ${props.title_class}`}>{props.title}</div>
+            <div className={`vertical-list-body ${props.list_class}`}>
                 {
                     props.contents.map(body => {
                         return <List body={body} />
