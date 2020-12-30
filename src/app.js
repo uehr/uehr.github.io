e40c7828a53fc8d3031b6f295b6a04009d472187
uehr <uehara.yudai@gmail.com>
2020-12-31 04:28:50 +0900

    add: season styles

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './assets/css/app.css';
+import './assets/css/season/spring.css';
+import './assets/css/season/summer.css';
+import './assets/css/season/autumn.css';
+import './assets/css/season/winter.css';
 import Header from './components/organisms/header.js';
 import Experiences from './components/organisms/experiences.js'
 import SNS from './components/organisms/sns.js'
@@ -22,6 +26,26 @@ function App() {
       <Footer />
     </div>
   );
+}
+
+window.onload = () => {
+    const month = new Date().getMonth() + 1
+    const app = document.getElementsByClassName("app")[0]
+
+    if(3 <= month && month <= 5) { // 春
+        console.log("spring style")
+        app.setAttribute("id", "spring")
+    }else if(6 <= month && month <= 8) { // 夏
+        console.log("summer style")
+        app.setAttribute("id", "summer")
+    }else if(9 <= month && month <= 11) { // 秋
+        console.log("autumn style")
+        app.setAttribute("id", "autumn")
+    }else if(12 <= month || month <= 2) { //冬
+        console.log("winter style")
+        // app.setAttribute("id", "winter")
+        app.setAttribute("id", "autumn")
+    }
 }
 
 export default App;
