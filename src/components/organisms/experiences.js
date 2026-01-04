a7ffe03c9dd5395c03f3f6fdbbe9a2c86b22c65a
uehr <uehara.yudai@gmail.com>
2026-01-05 04:47:33 +0900

    update: sort experiences time desc

--- src/components/organisms/experiences.js
+++ src/components/organisms/experiences.js
@@ -16,14 +16,14 @@ function Header() {
             body={
                 <div className="text-center">
                     <div className="d-inline-block text-left default-font">
-                        <List body="2015 沖縄海洋ロボコンROV部門 特別賞" />
-                        <List body="2016 Google ウェブレンジャーPJ 奨励賞" />
-                        <List body="2017 N高等学校 入学" />
-                        <List body="2018 株式会社ドワンゴ インターン" />
-                        <List body="2019 GMOペパボ株式会社 インターン" />
-                        <List body="2019 株式会社オプティム インターン" />
-                        <List body="2020 N高等学校 卒業" />
                         <List body="2020 株式会社オプティム 入社" />
+                        <List body="2020 N高等学校 卒業" />
+                        <List body="2019 株式会社オプティム インターン" />
+                        <List body="2019 GMOペパボ株式会社 インターン" />
+                        <List body="2018 株式会社ドワンゴ インターン" />
+                        <List body="2017 N高等学校 入学" />
+                        <List body="2016 Google ウェブレンジャーPJ 奨励賞" />
+                        <List body="2015 沖縄海洋ロボコンROV部門 特別賞" />
                     </div>
                 </div>
             }
